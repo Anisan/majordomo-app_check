a0a7c1b3a21611930640eda286047484082f0097
Eraser <eraser1981@gmail.com>
2017-07-21 14:21:50 +0300

    fix view

--- modules/app_check/ch_checks_search.inc.php
+++ modules/app_check/ch_checks_search.inc.php
@@ -21,13 +21,11 @@
   // SEARCH RESULTS
   $res=SQLSelect("SELECT * FROM ch_checks");
   $total=count($res);
-  print_r($total);
   if ($total>0) {
    //paging($res, 100, $out); // search result paging
    for($i=0;$i<$total;$i++) {
     // some action for every record if required
     $res[$i]["totalSum"] = $res[$i]["totalSum"] / 100;
    }
-   echo $res;
    $out['RESULT']=$res;
   }
